f5225f67917f676943785a804d0abf551975ad84
monthalcantara <montival_junior@yahoo.com.br>
2021-01-24 08:57:20 -0300

    Commit inicial

---
+++ src/main/java/io/github/monthalcantara/ApiEstudoDynamoDb/ApiEstudoDynamoDbApplication.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.ApiEstudoDynamoDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEstudoDynamoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEstudoDynamoDbApplication.class, args);
+	}
+
+}
